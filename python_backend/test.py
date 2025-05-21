01c3fc2e3a9f507070e513f4408809242af72efd
Khizar Tariq <khizar.tariqq2001@gmail.com>
2025-05-20 23:59:27 -0400

    Creating training config

--- python_backend/test.py
+++ python_backend/test.py
@@ -6,10 +6,10 @@ def read_char_range(file_path, start, end):
         text = f.read()
     return text[start:end]
 
-#[3572,3616,"SYLLABUSITEM"],[3617,3676,"SYLLABUSITEM"],[3677,3737,"SYLLABUSITEM"],[3738,3756,"SYLLABUSITEM"],[3757,3804,"SYLLABUSITEM"]
-file_path = 'txts/20239_CSC324H5F_LEC0101.txt'
-start = 3572
-end = 3616
+#[2537,2576,"SYLLABUSITEM"],[2577,2616,"SYLLABUSITEM"],[2675,2713,"SYLLABUSITEM"],[2714,2770,"SYLLABUSITEM"],[2771,2810,"SYLLABUSITEM"],[2811,2876,"SYLLABUSITEM"],[2877,2945,"SYLLABUSITEM"],[2946,2979,"SYLLABUSITEM"],[2980,3046,"SYLLABUSITEM"],[3047,3133,"SYLLABUSITEM"],[3134,3183,"SYLLABUSITEM"]
+file_path = 'txts/20231_MAT301H5S_LEC0101.txt'
+start = 1772
+end = 1810
 
 chunk = read_char_range(file_path, start, end)
 print(f"Characters {start}-{end}:\n{chunk}")
